17d7d1b687bba8dac2f305062805cfc6cb22ce6a
AlanBraatz <146029256+AlanBraatz@users.noreply.github.com>
2023-11-05 21:50:12 -0600

    Update test_UWW.py

--- test_UWW.py
+++ test_UWW.py
@@ -3,7 +3,7 @@ import main  # Import the main.py module
 
  
 def test_hello_uww(capsys):
-  main_output = "Hello UWWW\n"
+  main_output = "Hello UWWWW\n"
   main.main()
   captured = capsys.readouterr()
   assert captured.out == main_output
