011a76bc38a5b750f46664d73f55d6784450ae40
AlanBraatz <146029256+AlanBraatz@users.noreply.github.com>
2023-11-06 03:44:15 +0000

    Commit3test

---
+++ test_UWW.py
@@ -0,0 +1,10 @@
+# test_main.py
+import main  # Import the main.py module
+
+ 
+def test_hello_uww(capsys):
+  main_output = "Hello UWWW\n"
+  main.main()
+  captured = capsys.readouterr()
+  assert captured.out == main_output
+
